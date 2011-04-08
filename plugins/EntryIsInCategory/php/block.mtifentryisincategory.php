c55d97ef12594c0fc4d6e922ebf7994fc17cce03
Alfasado Inc <webmaster@alfasado.jp>
2011-04-08 09:49:03 +0900

    Fixed php.

--- plugins/EntryIsInCategory/php/block.mtifentryisincategory.php
+++ plugins/EntryIsInCategory/php/block.mtifentryisincategory.php
@@ -1,23 +1,19 @@
 <?php
 function smarty_block_mtifentryisincategory ( $args, $content, &$ctx, &$repeat ) {
-    if (! isset( $content ) ) {
-        $entry = $ctx->stash( 'entry' );
-        $category = $ctx->stash( 'category' );
-        $places = $entry->placement( FALSE );
-        $is_in = FALSE;
-        if ( is_array( $places ) ) {
-            if ( isset ( $category ) ) {
-                foreach ( $places as $place ) {
-                    if ( $place->category_id == $category->id ) {
-                        $is_in = TRUE;
-                        break;
-                    }
+    $entry = $ctx->stash( 'entry' );
+    $category = $ctx->stash( 'category' );
+    $places = $entry->placement( FALSE );
+    $is_in = FALSE;
+    if ( is_array( $places ) ) {
+        if ( isset ( $category ) ) {
+            foreach ( $places as $place ) {
+                if ( $place->category_id == $category->id ) {
+                    $is_in = TRUE;
+                    break;
                 }
             }
         }
-        return $ctx->_hdlr_if( $args, $content, $ctx, $repeat, $is_in );
-    } else {
-        return $ctx->_hdlr_if( $args, $content, $ctx, $repeat );
     }
+    return $ctx->_hdlr_if( $args, $content, $ctx, $repeat, $is_in );
 }
 ?>
